92f57538fad816ceae22173ee0a49a53f38d69ea
Tianyu Liu <poohlty@sina.com>
2013-07-15 21:47:37 -0400

    load from index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var content;
+
+fs.readFile("index.html", function (err, data) {
+  content = data;
+});
+
+var buffer = new Buffer(content, "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello Startup-class!');
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
