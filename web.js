162f3174babb706c153ecd11a157bb10080449ec
Tianyu Liu <poohlty@sina.com>
2013-07-15 22:10:17 -0400

    change async pattern

--- web.js
+++ web.js
@@ -3,19 +3,17 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var content;
-
-fs.readFile("index.html", function (err, data) {
-  content = data;
-});
-
-var buffer = new Buffer(content, "utf-8");
-
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8"));
+  var buffer;
+
+  fs.readFile("index.html", function (err, data) {
+    if (err) throw err;
+    buffer = new Buffer(data);
+    response.send(buffer.toString("utf-8"));
+  });
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5050;
 
 app.listen(port, function() {
   console.log("Listening on " + port);
